1a25667f2a6ef21aeb02030c01cef14d89f36462
Vincent Emonet <vincent.emonet@gmail.com>
2021-05-20 12:09:32 +0200

    implement output formatting

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse, RedirectResponse, PlainTextResponse
 
+import json
 import rdflib
 from rdflib.plugins.sparql.parser import Query
 from rdflib.plugins.sparql.processor import translateQuery
@@ -39,44 +40,53 @@ app = FastAPI(
 )
 def sparql_endpoint(
     request: Request,
-    query: Optional[str] = "SELECT * WHERE { <https://identifiers.org/OMIM:246300> <https://w3id.org/biolink/vocab/treated_by> ?drug . }"):
+    query: Optional[str]):
+    # query: Optional[str] = "SELECT * WHERE { <https://identifiers.org/OMIM:246300> <https://w3id.org/biolink/vocab/treated_by> ?drug . }"):
     # def sparql_query(query: Optional[str] = None):
     """
     Send a SPARQL query to be executed. 
     - Example with a drug: https://identifiers.org/DRUGBANK:DB00394
     - Example with a disease: https://identifiers.org/OMIM:246300
+    Example with custom concat function:
+    ```
+    PREFIX custom: <//custom/>
+
+    SELECT ?label1 ?label2 ?concat WHERE {
+        BIND("Hello" AS ?label1)
+        BIND("World" AS ?label2)
+        BIND(custom:openpredict(?label1, ?label2) AS ?concat)
+    }
+    ```
     \f
     :param query: SPARQL query input.
     """
     if not query:
         # TODO: return the SPARQL enndpoint service description
         return {"SPARQL Service": "description"}
+    output_format = 'json'
+
+    parsed_query = translateQuery(Query.parseString(query, parseAll=True))
+    query_operation = re.sub(r"(\w)([A-Z])", r"\1 \2", parsed_query.algebra.name)
+    if query_operation != "Select Query":
+        return JSONResponse(status_code=501, content={"message": str(query_operation) + " not implemented"})
     
+    print(parsed_query)
+    print(query_operation)
+    # predictions_list = query_classifier_from_sparql(parsed_query)
+    
+    # Docs rdflib custom fct: https://rdflib.readthedocs.io/en/stable/intro_to_sparql.html
+    # StackOverflow: https://stackoverflow.com/questions/43976691/custom-sparql-functions-in-rdflib/66988421#66988421
+    # Another project: https://github.com/bas-stringer/scry/blob/master/query_handler.py
+
+    # Save custom function in custom evaluation dictionary.
+    rdflib.plugins.sparql.CUSTOM_EVALS['SPARQL_openpredict'] = SPARQL_openpredict
+    query_results = rdflib.Graph().query(query)
+
+    print(query_results.serialize(format = 'json'))
     if request.headers['accept'] == 'text/csv':
-        # TODO: return in CSV format
-        return Response('a,b,c', media_type='text/csv')
+        return Response(query_results.serialize(format = 'csv'), media_type='text/csv')
     else:
-        parsed_query = translateQuery(Query.parseString(query, parseAll=True))
-        query_operation = re.sub(r"(\w)([A-Z])", r"\1 \2", parsed_query.algebra.name)
-        if query_operation != "Select Query":
-            return JSONResponse(status_code=501, content={"message": str(query_operation) + " not implemented"})
-        
-        print(parsed_query)
-        print(query_operation)
-        # predictions_list = query_classifier_from_sparql(parsed_query)
-        
-        # Docs rdflib custom fct: https://rdflib.readthedocs.io/en/stable/intro_to_sparql.html
-        # StackOverflow: https://stackoverflow.com/questions/43976691/custom-sparql-functions-in-rdflib/66988421#66988421
-        # Another project: https://github.com/bas-stringer/scry/blob/master/query_handler.py
-
-
-        # Save custom function in custom evaluation dictionary.
-        rdflib.plugins.sparql.CUSTOM_EVALS['SPARQL_openpredict'] = SPARQL_openpredict
-        predictions_list = []
-        for row in rdflib.Graph().query(query):
-            predictions_list.append(row)
-
-        return predictions_list
+        return json.loads(query_results.serialize(format = 'json'))
 
 
 @app.get("/", include_in_schema=False)
