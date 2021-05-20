ae1223938ade15b02284fa976fd4485bcd223532
Vincent Emonet <vincent.emonet@gmail.com>
2021-05-20 23:04:37 +0200

    Improve SPARQL endpoint

--- app/main.py
+++ app/main.py
@@ -203,8 +203,8 @@ async def post_sparql_endpoint(
         # import urllib.parse as urlparse
         # from urllib.parse import parse_qs
         # url = 'http://foo.appspot.com/abc?def=ghi'
-        # parsed = urlparse.urlparse(body)
-        parsed = parse.urlsplit(body)
+        parsed = urlparse.urlparse(body)
+        # parsed = parse.urlsplit(body)
         print('parsed query')
         print(parsed)
         query = parse.parse_qs(parsed.query)
