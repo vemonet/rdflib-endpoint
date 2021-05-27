df20f27e78305dd73a1a62ebfa16f4f6c071d0ad
Vincent Emonet <vincent.emonet@gmail.com>
2021-05-27 16:42:29 +0200

    add new example function: custom_concat

--- app/main.py
+++ app/main.py
@@ -23,6 +23,12 @@ from sparql_endpoint import SparqlEndpoint
 def most_similar(query_results, ctx, part, eval_part):
     """
     Get most similar entities for a given entity
+
+    Query:
+    PREFIX openpredict: <https://w3id.org/um/openpredict/>
+    SELECT ?drugOrDisease ?mostSimilar ?mostSimilarScore WHERE {
+        BIND("OMIM:246300" AS ?drugOrDisease)
+        BIND(openpredict:most_similar(?drugOrDisease) AS ?mostSimilar)
     """
     argument1 = str(_eval(part.expr.expr[0], eval_part.forget(ctx, _except=part.expr._vars)))
     argument2 = str(_eval(part.expr.expr[1], eval_part.forget(ctx, _except=part.expr._vars)))
@@ -47,10 +53,15 @@ def most_similar(query_results, ctx, part, eval_part):
     return query_results, ctx, part, eval_part
 
 
-
 def get_predictions(query_results, ctx, part, eval_part):
     """
     Query OpenPredict classifier to get drug/disease predictions
+
+    Query:
+    PREFIX openpredict: <https://w3id.org/um/openpredict/>
+    SELECT ?drugOrDisease ?predictedForTreatment ?predictedForTreatmentScore WHERE {
+        BIND("OMIM:246300" AS ?drugOrDisease)
+        BIND(openpredict:prediction(?drugOrDisease) AS ?predictedForTreatment)
     """
     argument1 = str(_eval(part.expr.expr[0], eval_part.forget(ctx, _except=part.expr._vars)))
 
@@ -74,11 +85,46 @@ def get_predictions(query_results, ctx, part, eval_part):
     return query_results, ctx, part, eval_part
 
 
+def custom_concat(query_results, ctx, part, eval_part):
+    """
+    Concat 2 string and return the length as additional Length variable
+
+    Query:
+    PREFIX myfunctions: <https://w3id.org/um/sparql-functions/>
+    SELECT ?concat ?concatLength WHERE {
+        BIND("First" AS ?first)
+        BIND(myfunctions:custom_concat(?first, "last") AS ?concat)
+    }
+    """
+    argument1 = str(_eval(part.expr.expr[0], eval_part.forget(ctx, _except=part.expr._vars)))
+    argument2 = str(_eval(part.expr.expr[1], eval_part.forget(ctx, _except=part.expr._vars)))
+    evaluation = []
+    scores = []
+
+    concat_string = argument1 + argument2
+    evaluation.append(concat_string)
+    scores.append(len(concat_string))
+    # Append the results for our custom function
+    for i, result in enumerate(evaluation):
+        query_results.append(eval_part.merge({
+            part.var: Literal(result), 
+            rdflib.term.Variable(part.var + 'Length'): Literal(scores[i])
+        }))
+    return query_results, ctx, part, eval_part
+
+
+
 # Start the SPARQL endpoint based on a RDFLib Graph
 g = Graph()
 app = SparqlEndpoint(
     graph=g,
     functions={
         'https://w3id.org/um/openpredict/prediction': get_predictions,
-        'https://w3id.org/um/openpredict/most_similar': most_similar
-    })
+        'https://w3id.org/um/openpredict/most_similar': most_similar,
+        'https://w3id.org/um/sparql-functions/custom_concat': custom_concat
+    },
+    title="SPARQL endpoint for RDFLib graph", 
+    description="A SPARQL endpoint to serve machine learning models, or any other logic implemented in Python. \n[Source code](https://github.com/vemonet/rdflib-endpoint)",
+    version="0.0.1",
+    cors_enabled=True
+)
