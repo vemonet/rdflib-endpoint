7f1c9d102083d91a68eb08e04a50245f601eb202
Vincent Emonet <vincent.emonet@gmail.com>
2022-02-14 12:00:18 +0100

    aiofiles missing?

--- rdflib_endpoint/sparql_endpoint.py
+++ rdflib_endpoint/sparql_endpoint.py
@@ -246,6 +246,7 @@ SELECT ?concat ?concatLength WHERE {
         @self.get("/", include_in_schema=False)
         async def serve_yasgui():
             """Serve YASGUI interface"""
+            print(pkg_resources.resource_filename('rdflib_endpoint', 'yasgui.html'))
             return FileResponse(pkg_resources.resource_filename('rdflib_endpoint', 'yasgui.html'))
 
 
