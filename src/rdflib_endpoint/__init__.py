f7c497b89e440f4c3abd8a5e0f2c6b713a4d02a5
Vincent Emonet <vincent.emonet@gmail.com>
2022-12-19 13:14:58 +0100

    update pkg version

--- src/rdflib_endpoint/__init__.py
+++ src/rdflib_endpoint/__init__.py
@@ -1,5 +1,5 @@
 """A package to deploy SPARQL endpoint to serve local RDF files, machine learning models, or any other logic implemented in Python, using RDFLib and FastAPI."""
 
-__version__ = "0.1.6"
+__version__ = "0.2.0"
 
 from .sparql_endpoint import SparqlEndpoint
