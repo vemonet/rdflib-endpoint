603d334d7a036e85c4a6682d0bcb727f8ab416cf
Vincent Emonet <vincent.emonet@gmail.com>
2023-02-24 08:20:42 +0100

    fix mypy

--- src/rdflib_endpoint/sparql_endpoint.py
+++ src/rdflib_endpoint/sparql_endpoint.py
@@ -120,9 +120,9 @@ SELECT ?concat ?concatLength WHERE {
         }
 
         @self.middleware("http")
-        async def add_process_time_header(request: Request, call_next):
+        async def add_process_time_header(request: Request, call_next: Any) -> Response:
             start_time = time.time()
-            response = await call_next(request)
+            response: Response = await call_next(request)
             process_time = time.time() - start_time
             response.headers["X-Process-Time"] = str(process_time)
             return response
